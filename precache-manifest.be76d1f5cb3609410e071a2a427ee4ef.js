eb2163deb7b581d0af3285a1e2912d5483cde1d1
iamsubroto <smartcoder500@gmail.com>
2018-12-09 22:10:31 -0800

    Updates

---
+++ precache-manifest.be76d1f5cb3609410e071a2a427ee4ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "cf22825e65217e1e4dba",
+    "url": "/contactmanager/static/js/main.cf22825e.chunk.js"
+  },
+  {
+    "revision": "0e5b2305205eca8f8f8f",
+    "url": "/contactmanager/static/js/1.0e5b2305.chunk.js"
+  },
+  {
+    "revision": "cf22825e65217e1e4dba",
+    "url": "/contactmanager/static/css/main.f8aadf53.chunk.css"
+  },
+  {
+    "revision": "8a181051cc8d2257fa871ea1cd8ef61c",
+    "url": "/contactmanager/index.html"
+  }
+];
